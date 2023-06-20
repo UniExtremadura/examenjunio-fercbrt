7b54c1f6e66d756b248fc3f6271064eaad162a77
fercbrt <74541334+fercbrt@users.noreply.github.com>
2023-06-20 18:18:08 +0200

    PEX10-5 IT5 - CU15 - IMP - Implement Components - DS

--- app/src/main/java/es/unex/asee/frojomar/asee_ses/utils/DateUtils.java
+++ app/src/main/java/es/unex/asee/frojomar/asee_ses/utils/DateUtils.java
@@ -15,6 +15,7 @@ public class DateUtils {
 
     //private static final String FORMAT_DATE="dd-MMM-yyyy,hh:mm aa";
     //Simulación de implementacion del CU15 Implement Components Rol AS
+    //Simulacion de implementación del CU16 Implement Components Rol DS
     private static final String FORMAT_TIME = "HH:mm:ss:SSS";
     private static final String FORMAT_DATE = "d-MMM-yyyy,HH:mm:ss aa";
     private static final String FORMAT_DATE_2 = "yyyy-MM-dd'T'hh:mm";
